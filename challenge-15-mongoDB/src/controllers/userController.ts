4f9243b3c2af66a5a31ab36e57e4b66aa5cf5b50
Jen <jensavino26@gmail.com>
2025-04-12 16:22:46 -0500

    troubleshooting routes in insomnia

--- challenge-15-mongoDB/src/controllers/userController.ts
+++ challenge-15-mongoDB/src/controllers/userController.ts
@@ -3,11 +3,107 @@ import User from '../models/User';
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
-    const users = await User.find().populate('thoughts').populate('friends');
-    res.json(users);
+    const users = await User.find();
+    res.json(users); // Should return an empty array if no users exist
   } catch (err) {
     res.status(500).json(err);
   }
 };
 
-// Add other CRUD operations (getUserById, createUser, updateUser, deleteUser, addFriend, removeFriend)
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// Get a single user by ID
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate('thoughts')
+      .populate('friends');
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user found with that ID' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// Update a user by ID
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user found with that ID' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// Delete a user by ID
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user found with that ID' });
+    }
+
+    res.json({ message: 'User deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// Add a friend
+export const addFriend = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// Remove a friend
+export const removeFriend = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- challenge-15-mongoDB/src/models/index.ts
+++ challenge-15-mongoDB/src/models/index.ts
@@ -1,2 +1,3 @@
 export { default as User } from './User';
 export { default as Thought } from './Thought';
+export { default as Reaction } from './Reaction';

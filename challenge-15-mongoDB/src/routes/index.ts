d550c4bc1998e5d5c1456850eba6c8ffcf9c8b39
Jen <jensavino26@gmail.com>
2025-04-12 19:46:16 -0500

    final insomnia troubleshoot

--- challenge-15-mongoDB/src/routes/index.ts
+++ challenge-15-mongoDB/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import userRoutes from './api/userRoutes';
+import thoughtRoutes from './api/thoughtRoutes';
 
 const router = Router();
 
 router.use('/users', userRoutes); // This maps /api/users to userRoutes
+router.use('/thoughts', thoughtRoutes);
+
 console.log('User routes registered');
-console.log('Routes registered: /api/users');
+console.log('Thought routes registered');
 
 export default router;
